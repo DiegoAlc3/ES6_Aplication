95f8fd8fdefc2590815556e445a5ea47bf1f20d4
Diego Alcantara <diegosalc15@gmail.com>
2019-12-14 11:24:59 -0300

    Render in window

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ class App{
   constructor(){
     this.repositories = [];
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
     this.registerHandlers();
   }
   registerHandlers(){
@@ -16,7 +17,32 @@ class App{
       avatar_url:'https://avatars0.githubusercontent.com/u/28929274?v=4',
       html_url:'https://github.com/DiegoAlc3/ES6_Aplication',
     });
-    console.log(this.repositories);
+    this.render();
+  }
+  render(){
+    this.listEl.innerHTML = '';
+    this.repositories.forEach(repo =>{
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target','_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    })
   }
 }
 
